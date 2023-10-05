9c3ada945db3c5971b9adddc439af60264d474a8
Nils Olav Handegard <nils.olav.handegard@imr.no>
2023-10-05 15:48:38 +0200

    Tracking script updated.

--- examples/tracking.py
+++ examples/tracking.py
@@ -1,275 +1,83 @@
+from KoronaScript import *
+
 # Tracking example
 
 # Below is the korona steps for tracking on individual frequencies
 
 # The data we need to get out is the calibrated pulse compressed samples
 # that belongs to each track and then calculate the TS(f)
+# Set lsss env variable
 
-
+lsss = '/home/nilsolav/lsss/lsss-2.16.0-alpha/'
+os.environ["LSSS"] = lsss
+inputdir = '/mnt/c/DATA/crimac/2023/T2023001/ACOUSTIC/EK80/EK80_RAWDATA/'
+outputdir = '/mnt/c/DATAscratch/korona/'
 '''
-<?xml version="1.0" encoding="UTF-8"?>
-
-<ConfigFiles context="Korona">
-   <parameter name="ModuleConfiguration" ref="CfsDirectory">KoronaModuleSetup_Tracking.cds</parameter>
-   <parameter name="Categorization" ref="CfsDirectory">categorization.xml</parameter>
-   <parameter name="HorizontalTransducerOffsets" ref="CfsDirectory">HorizontalTransducerOffsets.xml</parameter>
-   <parameter name="VerticalTransducerOffsets" ref="CfsDirectory">VerticalTransducerOffsets.xml</parameter>
-   <parameter name="TransducerRanges" ref="CfsDirectory">TransducerRanges.xml</parameter>
-   <parameter name="Plankton" ref="CfsDirectory">Plankton.xml</parameter>
-   <parameter name="BroadbandNotchFilters" ref="CfsDirectory">BroadbandNotchFilters.xml</parameter>
-   <parameter name="PulseCompressionFilters" ref="CfsDirectory">PulseCompressionFilters.xml</parameter>
-   <parameter name="BroadbandSplitterBands" ref="CfsDirectory">BroadbandSplitterBands.xml</parameter>
-   <parameter name="Towfish"/>
-</ConfigFiles>
-
-
-<?xml version="1.0" encoding="UTF-8"?>
-
-<ModuleContainer version="4">
-   <modules>
-      <module name="CommentModule">
-         <parameters>
-            <parameter name="Active">true</parameter>
-            <parameter name="LineBreak">false</parameter>
-            <parameter name="Label">Tracking setup:</parameter>
-            <parameter name="Comment"/>
-         </parameters>
-      </module>
-      <module name="EmptyPingRemovalModule">
-         <parameters>
-            <parameter name="Active">true</parameter>
-         </parameters>
-      </module>
-      <module name="DataReductionModule">
-         <parameters>
-            <parameter name="Active">true</parameter>
-            <parameter name="BlindZone">false</parameter>
-            <parameter name="MinRange">14</parameter>
-            <parameter name="MinDepth"/>
-            <parameter name="TransducerRange">false</parameter>
-            <parameter name="MaxRange">26</parameter>
-            <parameter name="MaxDepth"/>
-         </parameters>
-      </module>
-      <module name="TrackingModule">
-         <parameters>
-            <parameter name="Active">true</parameter>
-            <parameter name="TrackerType">SED</parameter>
-            <parameter name="kHz">333</parameter>
-            <parameter name="PlatformMotionType">Floating</parameter>
-            <parameter name="MinTS">-50</parameter>
-            <parameter name="PulseLengthDeterminationLevel">5</parameter>
-            <parameter name="MinEchoLength">0.005</parameter>
-            <parameter name="MaxEchoLength">0.15</parameter>
-            <parameter name="MaxGainCompensation">9</parameter>
-            <parameter name="DoPhaseDeviationCheck">false</parameter>
-            <parameter name="MaxPhaseDevSteps">10</parameter>
-            <parameter name="MaxTS">-10</parameter>
-            <parameter name="MaxDepth">27</parameter>
-            <parameter name="MaxAlongshipAngle">5</parameter>
-            <parameter name="MaxAthwartshipAngle">5</parameter>
-            <parameter name="InitiationGateFunction">
-               <parameter name="Alpha">2.8</parameter>
-               <parameter name="Beta">2.8</parameter>
-               <parameter name="Range">0.1</parameter>
-               <parameter name="TS">20</parameter>
-            </parameter>
-            <parameter name="InitiationMinLength">1</parameter>
-            <parameter name="GateFunction">
-               <parameter name="Alpha">2.8</parameter>
-               <parameter name="Beta">2.8</parameter>
-               <parameter name="Range">0.1</parameter>
-               <parameter name="TS">20</parameter>
-            </parameter>
-            <parameter name="AlphaBetaEstimator">
-               <parameter name="Alpha">0.9</parameter>
-               <parameter name="Beta">0.1</parameter>
-            </parameter>
-            <parameter name="MaxMissingPings">4</parameter>
-            <parameter name="MaxMissingSamples">2</parameter>
-            <parameter name="MaxMissingPingsFraction">0.7</parameter>
-            <parameter name="MinTrackLength">10</parameter>
-            <parameter name="MinSampleToLengthFraction">1</parameter>
-         </parameters>
-      </module>
-      <module name="TrackingModule">
-         <parameters>
-            <parameter name="Active">true</parameter>
-            <parameter name="TrackerType">SED</parameter>
-            <parameter name="kHz">200</parameter>
-            <parameter name="PlatformMotionType">Floating</parameter>
-            <parameter name="MinTS">-50</parameter>
-            <parameter name="PulseLengthDeterminationLevel">9</parameter>
-            <parameter name="MinEchoLength">0.005</parameter>
-            <parameter name="MaxEchoLength">0.15</parameter>
-            <parameter name="MaxGainCompensation">9</parameter>
-            <parameter name="DoPhaseDeviationCheck">false</parameter>
-            <parameter name="MaxPhaseDevSteps">10</parameter>
-            <parameter name="MaxTS">-10</parameter>
-            <parameter name="MaxDepth">27</parameter>
-            <parameter name="MaxAlongshipAngle">7</parameter>
-            <parameter name="MaxAthwartshipAngle">7</parameter>
-            <parameter name="InitiationGateFunction">
-               <parameter name="Alpha">2.8</parameter>
-               <parameter name="Beta">2.8</parameter>
-               <parameter name="Range">0.1</parameter>
-               <parameter name="TS">20</parameter>
-            </parameter>
-            <parameter name="InitiationMinLength">1</parameter>
-            <parameter name="GateFunction">
-               <parameter name="Alpha">2.8</parameter>
-               <parameter name="Beta">2.8</parameter>
-               <parameter name="Range">0.1</parameter>
-               <parameter name="TS">20</parameter>
-            </parameter>
-            <parameter name="AlphaBetaEstimator">
-               <parameter name="Alpha">0.9</parameter>
-               <parameter name="Beta">0.1</parameter>
-            </parameter>
-            <parameter name="MaxMissingPings">1</parameter>
-            <parameter name="MaxMissingSamples">1</parameter>
-            <parameter name="MaxMissingPingsFraction">0.7</parameter>
-            <parameter name="MinTrackLength">10</parameter>
-            <parameter name="MinSampleToLengthFraction">1</parameter>
-         </parameters>
-      </module>
-      <module name="TrackingModule">
-         <parameters>
-            <parameter name="Active">true</parameter>
-            <parameter name="TrackerType">SED</parameter>
-            <parameter name="kHz">120</parameter>
-            <parameter name="PlatformMotionType">Floating</parameter>
-            <parameter name="MinTS">-50</parameter>
-            <parameter name="PulseLengthDeterminationLevel">5</parameter>
-            <parameter name="MinEchoLength">0.005</parameter>
-            <parameter name="MaxEchoLength">0.15</parameter>
-            <parameter name="MaxGainCompensation">9</parameter>
-            <parameter name="DoPhaseDeviationCheck">false</parameter>
-            <parameter name="MaxPhaseDevSteps">10</parameter>
-            <parameter name="MaxTS">-10</parameter>
-            <parameter name="MaxDepth">27</parameter>
-            <parameter name="MaxAlongshipAngle">5</parameter>
-            <parameter name="MaxAthwartshipAngle">5</parameter>
-            <parameter name="InitiationGateFunction">
-               <parameter name="Alpha">2.8</parameter>
-               <parameter name="Beta">2.8</parameter>
-               <parameter name="Range">0.1</parameter>
-               <parameter name="TS">20</parameter>
-            </parameter>
-            <parameter name="InitiationMinLength">1</parameter>
-            <parameter name="GateFunction">
-               <parameter name="Alpha">2.8</parameter>
-               <parameter name="Beta">2.8</parameter>
-               <parameter name="Range">0.1</parameter>
-               <parameter name="TS">20</parameter>
-            </parameter>
-            <parameter name="AlphaBetaEstimator">
-               <parameter name="Alpha">0.9</parameter>
-               <parameter name="Beta">0.1</parameter>
-            </parameter>
-            <parameter name="MaxMissingPings">4</parameter>
-            <parameter name="MaxMissingSamples">2</parameter>
-            <parameter name="MaxMissingPingsFraction">0.7</parameter>
-            <parameter name="MinTrackLength">10</parameter>
-            <parameter name="MinSampleToLengthFraction">1</parameter>
-         </parameters>
-      </module>
-      <module name="TrackingModule">
-         <parameters>
-            <parameter name="Active">true</parameter>
-            <parameter name="TrackerType">SED</parameter>
-            <parameter name="kHz">70</parameter>
-            <parameter name="PlatformMotionType">Floating</parameter>
-            <parameter name="MinTS">-50</parameter>
-            <parameter name="PulseLengthDeterminationLevel">5</parameter>
-            <parameter name="MinEchoLength">0.005</parameter>
-            <parameter name="MaxEchoLength">0.15</parameter>
-            <parameter name="MaxGainCompensation">9</parameter>
-            <parameter name="DoPhaseDeviationCheck">false</parameter>
-            <parameter name="MaxPhaseDevSteps">10</parameter>
-            <parameter name="MaxTS">-10</parameter>
-            <parameter name="MaxDepth">27</parameter>
-            <parameter name="MaxAlongshipAngle">5</parameter>
-            <parameter name="MaxAthwartshipAngle">5</parameter>
-            <parameter name="InitiationGateFunction">
-               <parameter name="Alpha">2.8</parameter>
-               <parameter name="Beta">2.8</parameter>
-               <parameter name="Range">0.05</parameter>
-               <parameter name="TS">20</parameter>
-            </parameter>
-            <parameter name="InitiationMinLength">2</parameter>
-            <parameter name="GateFunction">
-               <parameter name="Alpha">2.8</parameter>
-               <parameter name="Beta">2.8</parameter>
-               <parameter name="Range">0.1</parameter>
-               <parameter name="TS">20</parameter>
-            </parameter>
-            <parameter name="AlphaBetaEstimator">
-               <parameter name="Alpha">0.9</parameter>
-               <parameter name="Beta">0.1</parameter>
-            </parameter>
-            <parameter name="MaxMissingPings">4</parameter>
-            <parameter name="MaxMissingSamples">2</parameter>
-            <parameter name="MaxMissingPingsFraction">0.7</parameter>
-            <parameter name="MinTrackLength">10</parameter>
-            <parameter name="MinSampleToLengthFraction">1</parameter>
-         </parameters>
-      </module>
-      <module name="TrackingModule">
-         <parameters>
-            <parameter name="Active">true</parameter>
-            <parameter name="TrackerType">SED</parameter>
-            <parameter name="kHz">38</parameter>
-            <parameter name="PlatformMotionType">Floating</parameter>
-            <parameter name="MinTS">-50</parameter>
-            <parameter name="PulseLengthDeterminationLevel">5</parameter>
-            <parameter name="MinEchoLength">0.005</parameter>
-            <parameter name="MaxEchoLength">1.5</parameter>
-            <parameter name="MaxGainCompensation">9</parameter>
-            <parameter name="DoPhaseDeviationCheck">false</parameter>
-            <parameter name="MaxPhaseDevSteps">8</parameter>
-            <parameter name="MaxTS">-10</parameter>
-            <parameter name="MaxDepth">27</parameter>
-            <parameter name="MaxAlongshipAngle">5</parameter>
-            <parameter name="MaxAthwartshipAngle">5</parameter>
-            <parameter name="InitiationGateFunction">
-               <parameter name="Alpha">2.8</parameter>
-               <parameter name="Beta">2.8</parameter>
-               <parameter name="Range">0.1</parameter>
-               <parameter name="TS">20</parameter>
-            </parameter>
-            <parameter name="InitiationMinLength">1</parameter>
-            <parameter name="GateFunction">
-               <parameter name="Alpha">2.8</parameter>
-               <parameter name="Beta">2.8</parameter>
-               <parameter name="Range">0.1</parameter>
-               <parameter name="TS">20</parameter>
-            </parameter>
-            <parameter name="AlphaBetaEstimator">
-               <parameter name="Alpha">0.9</parameter>
-               <parameter name="Beta">0.1</parameter>
-            </parameter>
-            <parameter name="MaxMissingPings">4</parameter>
-            <parameter name="MaxMissingSamples">2</parameter>
-            <parameter name="MaxMissingPingsFraction">0.7</parameter>
-            <parameter name="MinTrackLength">10</parameter>
-            <parameter name="MinSampleToLengthFraction">1</parameter>
-         </parameters>
-      </module>
-      <module name="TsDetectionModule">
-         <parameters>
-            <parameter name="Active">true</parameter>
-            <parameter name="DetectorType">PEAK</parameter>
-            <parameter name="MinTS">-50</parameter>
-            <parameter name="PulseLengthDeterminationLevel">6</parameter>
-            <parameter name="MinEchoLength">0.005</parameter>
-            <parameter name="MaxEchoLength">0.15</parameter>
-            <parameter name="MaxGainCompensation">4</parameter>
-            <parameter name="DoPhaseDeviationCheck">false</parameter>
-            <parameter name="MaxPhaseDevSteps">8</parameter>
-            <parameter name="MaxDepth">300</parameter>
-         </parameters>
-      </module>
-   </modules>
-</ModuleContainer>
+# Input
+if len(sys.argv) != 3:
+    print(f'Usage: {sys.argv[0]} inputdir outputdir')
+    exit(0)
+else:
+    inputdir = sys.argv[1]
+    outputdir = sys.argv[2]
 '''
+
+
+# Instanitate the class
+ks = KoronaScript()
+
+ks.add(Comment(Active = "true",
+               LineBreak = "false",
+               Label = "Tracking setup",
+               Comment = ""))
+
+ks.add(EmptyPingRemoval(Active = "true"))
+
+ks.add(DataReduction(Active = "true",
+                     BlindZone = "false",
+                     MinRange = "14",
+                     MinDepth = "",
+                     TransducerRange = "false",
+                     MaxRange = "26",
+                     MaxDepth = ""))
+
+ks.add(Tracking(Active = "true",
+                TrackerType = "SED",
+                kHz = "38",
+                PlatformMotionType = "Floating",
+                MinTS = "-50",
+                PulseLengthDeterminationLevel = "5",
+                MinEchoLength = "0.005",
+                MaxEchoLength = "0.15",
+                MaxGainCompensation = "9",
+                DoPhaseDeviationCheck = "false",
+                MaxPhaseDevSteps = "10",
+                MaxTS = "-10",
+                MaxDepth = "27",
+                MaxAlongshipAngle = "5",
+                MaxAthwartshipAngle = "5",
+                InitiationGateFunction = {
+                    "Alpha": 2.8,
+                    "Beta": 2.8,
+                    "Range": 0.1,
+                    "TS": 20},
+                InitiationMinLength = "1",
+                GateFunction = {
+                    "Alpha": 2.8,
+                    "Beta": 2.8,
+                    "Range": 0.1,
+                    "TS": 20},
+                AlphaBetaEstimator = {
+                    "Alpha": 0.9,
+                    "Beta": 0.1},
+                MaxMissingPings = "4",
+                MaxMissingSamples = "2",
+                MaxMissingPingsFraction = "0.7",
+                MinTrackLength = "10",
+                MinSampleToLengthFraction = "1"))
+
+# run korona
+ks.write()
+ks.run(src=inputdir, dst=outputdir) # Begrening på kjernar
+
+# Plot results
