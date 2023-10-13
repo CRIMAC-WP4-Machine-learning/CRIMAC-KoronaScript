74eb893e6a7e83240310e934966051b04541618a
Ketil Malde <ketil.malde@imr.no>
2023-10-13 13:14:45 +0200

    scripting fixes

--- test.sh
+++ test.sh
@@ -7,4 +7,4 @@ export LSSS=lsss-2.15.0-alpha
 
 mkdir -p test_pc.out
 
-python3 examples/pulseCompression.py /data/crimac/2023/T2023001/ACOUSTIC/EK80/EK80_RAWDATA/ test_pc.out
+python3 examples/pulseCompression.py /data/crimac/2023/T2023001/ACOUSTIC/EK80/EK80_RAWDATA/ test_pc.out $LSSS
