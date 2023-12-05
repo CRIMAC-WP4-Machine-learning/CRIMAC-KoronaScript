f31feb26350ef3b7a4a5c36f768f56eab0493138
Ketil Malde <ketil.malde@imr.no>
2023-12-05 14:29:27 +0100

    trim down testFMtoPC a bit

--- test.sh
+++ test.sh
@@ -3,8 +3,20 @@
 # Run a set of tests
 
 export PYTHONPATH=.
-export LSSS=lsss-2.15.0-alpha
+export LSSS=lsss-2.16.0-alpha
+export TESTSET=/data/crimac/2023/T2023001/ACOUSTIC/EK80/EK80_RAWDATA/
+# This is the messor data example (for FM2PC?):
+#    /data/crimac/2021/T2021002/ACOUSTIC/EK80/EK80_RAWDATA/
 
-mkdir -p test_pc.out
+mkdir -p test_cr.out
+python3 examples/channelRemoval.py $TESTSET test_cr.out
 
-python3 examples/pulseCompression.py /data/crimac/2023/T2023001/ACOUSTIC/EK80/EK80_RAWDATA/ test_pc.out $LSSS
+mkdir -p test_fm2pc.out
+python3 examples/testFMtoPC_hi.py $TESTSET test_fm2pc.out
+
+# mkdir -p test_pc.out
+# python3 examples/pulseCompression.py $TESTSET test_pc.out $LSSS
+# Fails
+
+# Too complicated to set up
+# python3 examples/processFMtestdata.py
