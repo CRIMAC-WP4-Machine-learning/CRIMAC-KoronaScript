f557bdb91f13059787745ab99e2f2f85c85e6059
Ketil Malde <ketil.malde@imr.no>
2023-12-05 14:18:28 +0100

    fix cr example to actually work

--- examples/channelRemoval.py
+++ examples/channelRemoval.py
@@ -2,16 +2,14 @@ import sys
 import KoronaScript as ks
 import KoronaScript.Modules as ksm
 
-ks = ks.KoronaScript(Categorization='categorization.xml',
-                     HorizontalTransducerOffsets=
-                     'HorizontalTransducerOffsets.xml')
+ks = ks.KoronaScript(TransducerRanges='examples/TransducerRanges.xml')
 
 ks.add(ksm.Tracking())
 ks.add(ksm.EmptyPingRemoval())
 ks.add(ksm.Comment(LineBreak='false', Label='CW_0256ms'))
-ks.add(ksm.TemporaryComputationsBegin(Active='passive'))
+ks.add(ksm.TemporaryComputationsBegin())
 ks.add(ksm.ChannelRemoval(Channels=[1,5,9,13,17],KeepSpecified='true'))
-ks.add(ksm.Writer(RelativeDirectory='CW_0256ms'))
+ks.add(ksm.Writer(RelativeDirectory='CW_0256ms', DirectoryName='cr_out'))
 ks.add(ksm.TemporaryComputationsEnd())
 
 if len(sys.argv) == 1:
