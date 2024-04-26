de81e4700418d4ac77ba9417cbfcdae6a0a970a6
Ketil Malde <ketil@malde.org>
2024-04-26 12:06:06 +0200

    Clean up test example

--- examples/testFMtoPC_hi.py
+++ examples/testFMtoPC_hi.py
@@ -1,22 +1,21 @@
 import KoronaScript.Modules as ksm
 import KoronaScript as ks
-import os
 import sys
 
-ksi = ks.KoronaScript(TransducerRanges = "examples/TransducerRanges.xml")
+ksi = ks.KoronaScript(TransducerRanges="examples/TransducerRanges.xml")
 
 # Add the pulsecompression module and write to nc
 
-ksi.add(ksm.TsDetection(Active = "true",
-                        DetectorType = "PEAK",
-                        MinTS = "-66",
-                        PulseLengthDeterminationLevel = "6",
-                        MinEchoLength = "0.01",
-                        MaxEchoLength = "1.8",
-                        MaxGainCompensation = "6",
-                        DoPhaseDeviationCheck = "true",
-                        MaxPhaseDevSteps = "8",
-                        MaxDepth = "40"))
+ksi.add(ksm.TsDetection(Active="true",
+                        DetectorType="PEAK",
+                        MinTS="-66",
+                        PulseLengthDeterminationLevel="6",
+                        MinEchoLength="0.01",
+                        MaxEchoLength="1.8",
+                        MaxGainCompensation="6",
+                        DoPhaseDeviationCheck="true",
+                        MaxPhaseDevSteps="8",
+                        MaxDepth="40"))
 
 ksi.write()
-ksi.run(src = sys.argv[1], dst = sys.argv[2])
+ksi.run(src=sys.argv[1], dst=sys.argv[2])
