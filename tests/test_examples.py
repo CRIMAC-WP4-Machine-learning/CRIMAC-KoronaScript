4498334a65f999a7d29148f92b8e0f1edc0c9782
Ketil Malde <ketil.malde@imr.no>
2025-08-22 13:34:02 +0200

    Use os.path.join instead of concat with slashes

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -3,7 +3,7 @@ import urllib.request as ftp
 
 import examples.channelRemoval as CR
 
-TESTDATA = 'test_data/EK60'
+TESTDATA = os.path.join('test_data', 'EK60')
 URL = 'ftp://ftp.ngdc.noaa.gov/pub/outgoing/mgg/wcd/pyEcholab_data/examples/EK60'
 
 def test_get_data():
@@ -11,7 +11,7 @@ def test_get_data():
     for suffix in ['bot', 'idx', 'raw']:
         for it in ['DY1201_EK60-D20120214-T231011', 'DY1706_EK60-D20170609-T005736']:
             fname = it + '.' + suffix
-            fullname = TESTDATA + '/' + fname
+            fullname = os.path.join(TESTDATA, fname)
             if not os.path.exists(fullname):
                 print(f'Fetching: {fname}')
                 ftp.urlretrieve(f'{URL}/{fname}', fullname)
