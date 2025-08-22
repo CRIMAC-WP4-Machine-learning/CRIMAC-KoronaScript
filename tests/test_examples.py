19a7c4427e6ad68b5354b1020ac21906f6451229
Ketil Malde <ketil@malde.org>
2025-08-22 13:01:28 +0200

    Update tests to avoid os.system call.

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,15 +1,21 @@
 import os
+import urllib.request as ftp
+
 import examples.channelRemoval as CR
 
 TESTDATA = 'test_data/EK60'
+URL = 'ftp://ftp.ngdc.noaa.gov/pub/outgoing/mgg/wcd/pyEcholab_data/examples/EK60'
 
 def test_get_data():
-    if not os.path.exists(TESTDATA):
-        # Get test data from pyEchoLab examples:
-        os.system('mkdir -p test_data && cd test_data && wget --recursive --no-parent -nH --cut-dirs=6 ftp://ftp.ngdc.noaa.gov/pub/outgoing/mgg/wcd/pyEcholab_data/examples/EK60')
+    os.makedirs(TESTDATA, exist_ok=True)
+    for suffix in ['bot', 'idx', 'raw']:
+        for it in ['DY1201_EK60-D20120214-T231011', 'DY1706_EK60-D20170609-T005736']:
+            fname = it + '.' + suffix
+            fullname = TESTDATA + '/' + fname
+            if not os.path.exists(fullname):
+                print(f'Fetching: {fname}')
+                ftp.urlretrieve(f'{URL}/{fname}', fullname)
 
 def test_channelRemoval():
     """This fails to fail if no data is present"""
     CR.ks.run('test_data/EK60', 'test_out/cr_out')
-
-
